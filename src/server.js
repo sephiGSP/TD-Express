5afd4167ac668b28710554a850f4c30b428cffac
sephiGSP <sephiroth49@live.com>
2018-08-07 10:41:45 +0200

    lecture de movies.json avec express

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 //const express = require('express') equivaut a la ligne du dessous
 import express from 'express'
+import fs from 'fs'
+
 const app = express()
+var monJson = JSON.parse(fs.readFileSync('movies.json', 'utf8'));
 
 app.get('/ping', function (req, res) {
   //res.status(200).setHeader('content-type', 'application/text').send('pong') equivalent a la ligne du dessous 
   res.send('Pong')
+})
+
+app.get('/movies', function (req, res) {
+    res.send(monJson)
 })
 
 app.listen(5000, function () {
